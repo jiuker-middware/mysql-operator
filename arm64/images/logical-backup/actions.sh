5da4970b2095b9f0d51ca5c5bdd55bc24994e3f4
drivebyer <wuyangmuc@gmail.com>
2024-03-11 16:00:29 +0800

    update PG logical backup

--- arm64/images/logical-backup/actions.sh
+++ arm64/images/logical-backup/actions.sh
@@ -10,6 +10,9 @@ action=${1:-"dump"}
 if [ "$action" = "delete" ]; then
     echo "Delete backup..."
     /bin/bash /delete.sh
+elif [ "$action" = "restore" ]; then
+    echo "Restore backup..."
+    /bin/bash /restore.sh
 else
     echo "Dump backup to remote..."
     /bin/bash /dump.sh
